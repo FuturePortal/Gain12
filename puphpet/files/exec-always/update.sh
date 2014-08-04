aba9ea3840860588a84bf5f3a403a7f70a01121a
Rick van der Staaij <info@futureportal.com>
2014-08-04 22:07:57 +0200

    Finish vagrant box setup

--- puphpet/files/exec-always/update.sh
+++ puphpet/files/exec-always/update.sh
@@ -6,6 +6,9 @@ echo '========================================================'
 
 cd /vagrant
 
+echo 'Making sure the config file is ready to rumble'
+yes n | cp -i gain12/config/config.php.pub gain12/config/config.php
+
 echo 'Updating composer'
 php composer.phar self-update
 php composer.phar install --dev
