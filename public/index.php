3b49d3f3cf4032894ffafc97ed48b390f0396570
Rick van der Staaij <info@futureportal.com>
2014-08-06 22:04:59 +0200

    Update readme, add composer.json and setup a page base

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'Hello world!';
+// Set document root
+chdir(dirname(__DIR__));
+
+// Setup the database connection
+require 'vendor/autoload.php';
+require 'gain12/config/config.php';
+require 'gain12/core/init.php';
+
+// Load the wanted page
+switch (empty($_GET['page']) ? 'home' : $_GET['page']) {
+    case 'home':
+        echo 'Homepage!';
+        break;
+    default:
+        echo 'Oh noes, 404!';
+        break;
+}
