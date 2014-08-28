636b73510c08341ba45be1f56a93eb1a76c8ef96
Rick van der Staaij <info@futureportal.com>
2014-08-29 00:03:50 +0200

    T-888 test

--- gain12/page/Home.php
+++ gain12/page/Home.php
@@ -18,8 +18,7 @@ class Home extends Page
         $crap = '';
         for ($i = 0; $i <= 100; $i++) {
             switch (rand(1, 5)) {
-                case 1: $crap .= 'lol ';
-                    break;
+                case 1: $crap .= 'lol '; break;
                 case 2:
                     $crap .= 'jizzle ';
                     break;
@@ -28,9 +27,7 @@ class Home extends Page
                     break;
                 case 4:
                     $crap .= 'super '; break;
-                case 5:
-                    $crap .= 'powers ';
-                    break;
+                case 5: $crap .= 'powers '; break;
             }
             $crap .= '<br />';
         }
