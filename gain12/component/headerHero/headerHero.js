e1e91dd8b5e5f89233450f00d92b93e55baab424
Rick van der Staaij <info@futureportal.com>
2014-10-08 22:53:58 +0200

    Keep header details centered & resize on size change

--- gain12/component/headerHero/headerHero.js
+++ gain12/component/headerHero/headerHero.js
@@ -6,5 +6,24 @@ HeroHeader.prototype = new Component();
 HeroHeader.prototype.constructor = HeroHeader;
 
 HeroHeader.prototype.init = function () {
+    this.setSize();
+
+    $(window).on('resize', function () {
+        HeroHeader.prototype.setSize();
+    });
+
+    $(window).on('scroll touchmove', function () {
+        HeroHeader.prototype.onScroll();
+    });
+};
+
+HeroHeader.prototype.onScroll = function () {
+    var scrollTop = $(document).scrollTop();
+    var wrap = $('#header-inner-wrap');
+
+    wrap.css('top', scrollTop + 'px');
+};
+
+HeroHeader.prototype.setSize = function () {
     $('#header').height($(window).height());
 };
