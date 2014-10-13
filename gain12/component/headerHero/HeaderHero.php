f01e6c370afdb6e57957ed4ce279d55ed043da3e
Rick van der Staaij <info@futureportal.com>
2014-10-13 22:40:14 +0200

    Set hero header component todo

--- gain12/component/headerHero/HeaderHero.php
+++ gain12/component/headerHero/HeaderHero.php
@@ -4,6 +4,8 @@ namespace gain12\component\HeaderHero;
 
 class HeaderHero extends \gain12\component\Component
 {
+    // @todo: Abstract all variables to setters to make this component reusable 
+
     public function getContent()
     {
         return $this->render(
