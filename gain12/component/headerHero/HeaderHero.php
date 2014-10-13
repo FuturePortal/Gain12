16e8ddd473d21b8a6a40176a62144e12be7216b5
Rick van der Staaij <info@futureportal.com>
2014-10-13 22:48:40 +0200

    Code style fix, remove whitespace

--- gain12/component/headerHero/HeaderHero.php
+++ gain12/component/headerHero/HeaderHero.php
@@ -4,7 +4,7 @@ namespace gain12\component\HeaderHero;
 
 class HeaderHero extends \gain12\component\Component
 {
-    // @todo: Abstract all variables to setters to make this component reusable 
+    // @todo: Abstract all variables to setters to make this component reusable
 
     public function getContent()
     {
